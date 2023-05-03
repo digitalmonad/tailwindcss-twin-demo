ec63ce5f408800a2bec08230cd6ca2338c1b4cbf
Pavel Kocman <kocmanpavel@gmail.com>
2023-05-03 14:21:21 +0200

    add Topbar and RightColumn

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,18 @@ import {
   Settings as SettingsIcon,
   Info as InfoIcon,
   Sun as SunIcon,
+  Moon as MoonIcon,
 } from 'react-feather';
 import { Tooltip } from './components/utils/Tooltip';
 import { ThemeContext } from './hooks/ThemeContext';
 import OverviewFlow from './components/utils/Overview';
 
 function App() {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <Root>
-      <Aside>
+      <LeftColumn>
         <BrandLogoWrapper>
           <StyledBrandLogo />
         </BrandLogoWrapper>
@@ -41,7 +42,7 @@ function App() {
               <ImageIcon />
             </NavLink>
           </Tooltip>
-          <Tooltip content='Export' position='right'>
+          <Tooltip content='Integrations' position='right'>
             <NavLink href='#'>
               <IntegrationsIcon />
             </NavLink>
@@ -60,27 +61,34 @@ function App() {
           </Tooltip>
           <Tooltip content='Switch Theme' position='right'>
             <NavLink onClick={toggleTheme}>
-              <SunIcon />
+              {theme === 'dark' ? <MoonIcon /> : <SunIcon />}
             </NavLink>
           </Tooltip>
         </SettingsAndHelp>
-      </Aside>
-      <Wrapper>
-        <OverviewFlow />
-      </Wrapper>
+      </LeftColumn>
+      <CenterColumn>
+        <Topbar></Topbar>
+        <WorkboardWrapper>
+          <OverviewFlow />
+        </WorkboardWrapper>
+      </CenterColumn>
+      <RightColumn></RightColumn>
     </Root>
   );
 }
 
 const Root = tw.div`flex bg-gray-100  dark:bg-gray-900 font-sans text-gray-900 dark:text-gray-200`;
-const Aside = tw.aside`flex h-screen flex-col w-18 border-r items-center border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 z-10`;
+const LeftColumn = tw.aside`flex h-screen flex-col w-18 border-r items-center border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 z-10`;
+const RightColumn = tw.aside`flex h-screen flex-col w-[300px] border-l items-center border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 z-10`;
+const Topbar = tw.aside`flex h-18 border-b w-full border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 z-10`;
+const CenterColumn = tw.div`flex flex-1 flex-col`;
+const WorkboardWrapper = tw.div`flex flex-1`;
 const Nav = tw.nav`flex flex-col gap-y-4 pt-10 flex-1`;
 const SettingsAndHelp = tw.nav`flex flex-none flex-col gap-y-4 pb-10 `;
-const NavLink = tw.a`relative cursor-pointer rounded-md bg-gray-100 dark:bg-gray-700 p-2 text-primary-600 dark:text-primary-200 hover:bg-gray-50`;
+const NavLink = tw.a`relative cursor-pointer rounded-md bg-gray-100 dark:bg-gray-700 p-2 text-primary-600 dark:text-primary-200 hover:bg-gray-50 drop-shadow-md dark:hover:bg-gray-600`;
 const BrandLogoWrapper = tw.div`flex border-b h-18 w-full items-center justify-center border-gray-200 dark:border-gray-700`;
 const StyledBrandLogo = tw(
   BrandLogo
 )`fill-primary-600 dark:fill-primary-200 w-8 h-8`;
-const Wrapper = tw.div`flex flex-1`;
 
 export default App;
